8e482aae0fba91e052af960a1809b73a6285aee5
Tomas Belskis <c11477418@mydit.ie>
2016-08-20 15:52:29 +0100

    Starting chp3, turle graphics example

---
+++ Chapter_3/Turtle.py
@@ -0,0 +1,10 @@
+import turtle #imports pythons turtle module
+
+window = turtle.Screen() #creates playground(scene/window) for turtles
+trl = turtle.Turtle() #creates turlet assigns to trl
+
+trl.forward(50) #moves trl forward 50
+trl.left(90) #turns trl 90 degrees
+trl.forward(30) #Complete the second side of a rectangle
+
+turtle.mainloop() #waits for user to close the window
