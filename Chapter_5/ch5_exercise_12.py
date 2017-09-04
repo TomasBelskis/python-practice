3a4470cd83a9a46e7370f66e08d752e30e3f5799
TomasLT <tomasbelskis.dev@gmail.com>
2017-09-04 21:44:33 +0100

    Finished chapter 5 examples

--- Chapter_5/ch5_exercise_12.py
+++ Chapter_5/ch5_exercise_12.py
@@ -11,9 +11,20 @@ def is_rightangled(s1,s2,s3):
     sqr_s2 = s2 ** 2
     sqr_s3 = s3 ** 2
 
-    if (abs((sqr_s1 + sqr_s2) - sqr_s3) < 0.000001):
-        return True
+    if (s3 > s2 > s1):
+        if (abs((sqr_s1 + sqr_s2) - sqr_s3) < 0.000001):
+            return True
+        else:
+            return False
+    elif (s2 > s3 > s1):
+        if (abs((sqr_s1 + sqr_s3) - sqr_s2) < 0.000001):
+            return True
+        else:
+            return False
     else:
-        return False
+        if (abs((sqr_s2 + sqr_s3) - sqr_s1) < 0.000001):
+            return True
+        else:
+            return False
 
-print(is_rightangled(3,4,5))
+print(is_rightangled(6,4,3))
