97ec882e0597c7aa8f3c2b68c4015a209cda0807
Tomas Belskis <c11477418@mydit.ie>
2017-09-07 22:34:32 +0100

    Finished chapter 6

--- Chapter_6/ch6_example_test_suite.py
+++ Chapter_6/ch6_example_test_suite.py
@@ -4,3 +4,46 @@
 - Testing code is important, this can be done by writing test cases in order
 to test functions.
 """
+
+import sys
+
+def absolute_value(x):
+    if x < 0:
+        return -x
+    return x
+
+def absolute_value_bug(n):# Buggy version
+    """ Compute the absolute value of n """
+    if n < 0:
+        return 1
+    elif n > 0:
+        return n
+
+def test(did_pass):
+    """ Print the result of the test. """
+    linenum = sys._getframe(1).f_lineno # Get the caller's line number
+    if did_pass:
+        msg = " Test at line {0} ok. ".format(linenum)
+    else:
+        msg = ("Test at line {0} FAILED.".format(linenum))
+    print(msg)
+
+def test_suite():
+    """ Run the suite of test for code in this module (this file).
+    """
+    test(absolute_value(17) == 17)
+    test(absolute_value(-17) == 17)
+    test(absolute_value(0) == 0)
+    test(absolute_value(3.14) == 3.14)
+    test(absolute_value(-3.14) == 3.14)
+    # Testing buggy version of the function
+    test(absolute_value_bug(17) == 17)
+    test(absolute_value_bug(-17) == 17)
+    test(absolute_value_bug(0) == 0)
+    test(absolute_value_bug(3.14) == 3.14)
+    test(absolute_value_bug(-3.14) == 3.14)
+
+test_suite() # Here is the call to run the tests
+# Tree test fail for the buggy version
+# TODO Read up on assertion and how to use instead of writing own test function
+
