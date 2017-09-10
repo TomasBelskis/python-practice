fb10f6abcdf3cb3e7cb3cd4127fd6a479a1b09ce
Tomas Belskis <tomasbelskis.dev@gmail.com>
2017-09-10 22:36:01 +0100

    Added test cases for exercise 2

--- Chapter_6/ch6_exercise_1.py
+++ Chapter_6/ch6_exercise_1.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 Condition: All of the exercises below should be added to a single file. In
            that file, you should also add the test and test_suite scaffolding
@@ -32,10 +33,19 @@ def turn_clockwise(direction):
         return "None"
     return "None"
 
+def day_name(num):
+    return "None"
+
 class test(unittest.TestCase):
     def setUp(self):
         pass
-
+    """
+        Exercise 1:The four compass points can be abbreviated by single-lett-
+        er strings as “N”, “E”, “S”, and “W”. Write a function turn_clockwise
+        that takes one of these four compass points as its parameter, and re-
+        turns the next compass point in the clockwise direction. Here are so-
+        me tests that should pass:
+    """
     def test_1(self):
         self.assertEqual(turn_clockwise("N"), "E")
 
@@ -47,6 +57,23 @@ class test(unittest.TestCase):
 
     def test_4(self):
         self.assertEqual(turn_clockwise("rubbish"), "None")
+
+    """End Exercise 1 test"""
+
+    """
+        Exercise 2: Write a function day_name that converts an integer number
+        0 to 6 into the name of a day. Assume day 0 is “Sunday”. Once again, 
+        return None if the arguments to the function are not valid. Here are 
+        some tests that should pass:
+    """
+    def test_1_day(self):
+        self.assertEqual(day_name(3), "Wednesday")
+
+    def test_2_day(self):
+        self.assertEqual(day_name(6), "Saturday")
+
+    def test_3_day(self):
+        self.assertEqual(day_name(42), "None")
 
     def tearDown(self):
         pass
