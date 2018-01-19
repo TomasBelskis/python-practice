233a05dc5e22fd30c2b4d18a5a279ff0365b766c
TomasLT <tomas.belskis@icloud.com>
2018-01-19 22:42:08 +0000

    Beginning chapter 8 exercises

---
+++ Chapter_8/ch8_exercise_1.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Description: 1. What is the result of each of the following:
+                1a."Python"[1]
+                1b."Strings are sequences of characters."[5]
+                1c.len("wonderful")
+                1d."Mystery"[:4]
+                1e."p" in "Pineapple"
+                1f."apple" in "Pineapple"
+                1g."pear" not in "Pineapple"
+                1h."apple" > "pineapple"
+                1k."pineapple" < "Peach"
+"""
+
+"""
+@Author: Tomas Belskis
+@Licence: MIT
+@Exercise: Chapter 8 exercise 1
+@Description: Exercises 1
+"""
+
+"""
+1a. 'y'
+1b. 'g'
+1c.  9
+1d. 'Myst'
+1e. True
+1f. True
+1g. True
+1h. False
+1k. False
+"""
