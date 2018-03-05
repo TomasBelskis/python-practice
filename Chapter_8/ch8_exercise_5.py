07e2855fa7280a83aec0ffa23a49620e71c457d6
TomasLT <tomas.belskis@icloud.com>
2018-03-05 22:55:39 +0000

    Exercise 5 chapter 8

---
+++ Chapter_8/ch8_exercise_5.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+@Author: Tomas Belskis
+@Licence: MIT
+@Exercise: Chapter 8
+@Description: Exercise 5: Assign to a variable in your program a triple-quoted
+                          string that contains your favourite paragraph of text
+                          — perhaps a poem, a speech, instructions to bake a cake,
+                          some inspirational verses, etc.
+"""
+
+paragraph = """ Two roads diverged in a yellow wood,
+                And sorry I could not travel both
+                And be one traveler, long I stood
+                And looked down one as far as I could
+                To where it bent in the undergrowth;
+                
+                Then took the other, as just as fair,
+                And having perhaps the better claim,
+                Because it was grassy and wanted wear;
+                Though as for that the passing there
+                Had worn them really about the same,
+                
+                And both that morning equally lay
+                In leaves no step had trodden black.
+                Oh, I kept the first for another day!
+                Yet knowing how way leads on to way,
+                I doubted if I should ever come back.
+                
+                I shall be telling this with a sigh
+                Somewhere ages and ages hence:
+                Two roads diverged in a wood, and I—
+                I took the one less traveled by,
+                And that has made all the difference."""
+
+print(paragraph)
